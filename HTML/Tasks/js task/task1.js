eef60bbc37423f8c1d5a7578f6002f83a23bb340
ShambhuMs <shambhu88.xworkz@gmail.com>
2024-02-12 22:42:14 +0530

    task

---
+++ HTML/Tasks/js task/task1.js
@@ -0,0 +1,122 @@
+var doornumber=101;
+console.log(doornumber);
+var isdoorbell=true;
+console.log(isdoorbell);
+var color='black';
+console.log(color);
+var size=30;
+console.log(size);
+var doornumber1=102;
+console.log(doornumber1);
+var isdoorbell1=true;
+console.log(isdoorbell1);
+var color1='black';
+console.log(color1);
+var size1=30;
+console.log(size1);
+var doornumber2=103;
+console.log(doornumber2);
+var isdoorbell2=false;
+console.log(isdoorbell2);
+
+
+let num=()=>{
+  let sun=45;
+  console.log(sun);
+}
+num();
+
+
+//door
+let type="sliding Door";
+let size=12;
+let security=true;
+const style="flush door";
+const price=3000;
+const material="wood";
+var color="Blue";
+var fireResistance=false;
+var locerSystem=true;
+
+console.log(type, typeof(type));
+console.log(size, typeof(size));
+console.log(security, typeof(security));
+console.log(style, typeof(style));
+console.log(price, typeof(price));
+console.log(material, typeof(material));
+console.log(color, typeof(color));
+console.log(fireResistance, typeof(fireResistance));
+console.log(locerSystem, typeof(locerSystem));
+console.log("========================================");
+
+//locker
+
+let lockerNumber = 101;
+let isLocked = false;
+let lockerContents = ['documents', 'jewelry', 'electronics'];
+const MAX_CONTENTS = 10;
+const LOCKER_SIZE = 'medium';
+const SECURITY_LEVEL = 'high';
+var lockerLocation = 'Main Floor';
+var isOpen = true;
+var accessCode = '1234';
+
+console.log("Locker properties");
+console.log("Locker Number:", lockerNumber, typeof(lockerNumber));
+console.log("Is Locked:", isLocked,  typeof(isLocked));
+console.log("Locker Contents:", lockerContents, typeof(lockerContents));
+console.log("Max Contents:", MAX_CONTENTS , typeof(MAX_CONTENTS));
+console.log("Locker Size:", LOCKER_SIZE , typeof(LOCKER_SIZE));
+console.log("Security Level:", SECURITY_LEVEL, typeof(SECURITY_LEVEL));
+console.log("Locker Location:", lockerLocation, typeof(lockerLocation));
+console.log("Is Open:", isOpen);
+console.log("Access Code:", accessCode);
+console.log("========================================");
+
+//Table
+let type1="Study Table";
+let size1=12;
+let security1=true;
+const style1="flush door";
+const price1=3000;
+const material1="wood";
+var color="Blue";
+var fireResistance=false;
+var locerSystem=true;
+
+
+console.log("'Table Properties'");
+console.log(type1, typeof(type1));
+console.log(size1, typeof(size1));
+console.log(security1, typeof(security1));
+console.log(style1, typeof(style1));
+console.log(price1, typeof(price1));
+console.log(material1, typeof(material1));
+console.log(color, typeof(color));
+console.log(fireResistance, typeof(fireResistance));
+console.log(locerSystem, typeof(locerSystem));
+console.log("========================================");
+
+//flug
+let flugName = "SkyRider";
+let flugSpeed = 800;
+let flugAltitude = 10000;
+const flugCapacity = 150;
+const flugFuelLevel = 75;
+const flugIsFlying = false;
+const MAX_CAPACITY = 200;
+var flugDestination = "Cityville";
+var flugManufacturer = "AeroTech";
+var flugStatus = "Grounded";
+
+console.log("Flug Properties");
+console.log(flugName, typeof(flugName));
+console.log(flugSpeed, typeof(flugSpeed));
+console.log(flugAltitude, typeof(flugAltitude));  
+console.log(flugCapacity, typeof(flugCapacity));
+console.log(flugFuelLevel, typeof(flugFuelLevel));
+console.log(flugIsFlying, typeof(flugIsFlying));
+console.log(MAX_CAPACITY, typeof(MAX_CAPACITY));
+console.log(flugDestination, typeof(flugDestination));
+console.log(flugManufacturer, typeof(flugManufacturer));
+console.log(flugStatus, typeof(flugStatus));
